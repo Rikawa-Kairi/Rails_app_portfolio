e341bf9e90d71418d7192f7204e0f2e4ca2c197c
Rikawa-Kairi <pckairi0127@gmail.com>
2022-03-19 23:53:57 +0900

    rails-migration-update-post-id

--- db/migrate/20220213213244_remove_post_from_entries.rb
+++
@@ -1,5 +0,0 @@
-class RemovePostFromEntries < ActiveRecord::Migration[6.0]
-  def change
-    add_column :entries, :post, :integer
-  end
-end
--- db/migrate/20220213220405_remove_postfrom_entries.rb
+++
@@ -1,5 +0,0 @@
-class RemovePostfromEntries < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :entries, :post, :integer
-  end
-end
